8bf42b311e3d7e0a633d2878d8d7f2f089688000
y2meta <y2meta@proton.me>
2023-05-18 15:08:51 +0530

    changes in alternative

--- src/components/alternative.js
+++ src/components/alternative.js
@@ -3,28 +3,28 @@ import React from "react";
 const Alternative = () => {
   return (
     <>
-      <link rel="alternate" hrefLang="en" href="https://www-y2mate.com/en10/" />
-      <link rel="alternate" hrefLang="de" href="https://www-y2mate.com/de/" />
-      <link rel="alternate" hrefLang="es" href="https://www-y2mate.com/es/" />
-      <link rel="alternate" hrefLang="fr" href="https://www-y2mate.com/fr/" />
-      <link rel="alternate" hrefLang="hi" href="https://www-y2mate.com/hi/" />
-      <link rel="alternate" hrefLang="id" href="https://www-y2mate.com/id/" />
-      <link rel="alternate" hrefLang="it" href="https://www-y2mate.com/it/" />
-      <link rel="alternate" hrefLang="ja" href="https://www-y2mate.com/ja/" />
-      <link rel="alternate" hrefLang="ko" href="https://www-y2mate.com/ko/" />
-      <link rel="alternate" hrefLang="my" href="https://www-y2mate.com/my/" />
-      <link rel="alternate" hrefLang="ms" href="https://www-y2mate.com/ms/" />
-      <link rel="alternate" hrefLang="tl-ph" href="https://www-y2mate.com/tl-ph/" />
-      <link rel="alternate" hrefLang="pt" href="https://www-y2mate.com/pt/" />
-      <link rel="alternate" hrefLang="ru" href="https://www-y2mate.com/ru/" />
-      <link rel="alternate" hrefLang="th" href="https://www-y2mate.com/th/" />
-      <link rel="alternate" hrefLang="tr" href="https://www-y2mate.com/tr/" />
-      <link rel="alternate" hrefLang="vi" href="https://www-y2mate.com/vi/" />
-      <link rel="alternate" hrefLang="zh-cn" href="https://www-y2mate.com/zh-cn/" />
-      <link rel="alternate" hrefLang="zh-tw" href="https://www-y2mate.com/zh-tw/" />
-      <link rel="alternate" hrefLang="ar" href="https://www-y2mate.com/ar/" />
-      <link rel="alternate" hrefLang="bn" href="https://www-y2mate.com/bn/" />
-      <link rel="alternate" hrefLang="x-default" href="https://www-y2mate.com/en10/" />
+      <link rel="alternate" hrefLang="en" href="https://mp3paw.mobi/en10/" />
+      <link rel="alternate" hrefLang="de" href="https://mp3paw.mobi/de/" />
+      <link rel="alternate" hrefLang="es" href="https://mp3paw.mobi/es/" />
+      <link rel="alternate" hrefLang="fr" href="https://mp3paw.mobi/fr/" />
+      <link rel="alternate" hrefLang="hi" href="https://mp3paw.mobi/hi/" />
+      <link rel="alternate" hrefLang="id" href="https://mp3paw.mobi/id/" />
+      <link rel="alternate" hrefLang="it" href="https://mp3paw.mobi/it/" />
+      <link rel="alternate" hrefLang="ja" href="https://mp3paw.mobi/ja/" />
+      <link rel="alternate" hrefLang="ko" href="https://mp3paw.mobi/ko/" />
+      <link rel="alternate" hrefLang="my" href="https://mp3paw.mobi/my/" />
+      <link rel="alternate" hrefLang="ms" href="https://mp3paw.mobi/ms/" />
+      <link rel="alternate" hrefLang="tl-ph" href="https://mp3paw.mobi/tl-ph/" />
+      <link rel="alternate" hrefLang="pt" href="https://mp3paw.mobi/pt/" />
+      <link rel="alternate" hrefLang="ru" href="https://mp3paw.mobi/ru/" />
+      <link rel="alternate" hrefLang="th" href="https://mp3paw.mobi/th/" />
+      <link rel="alternate" hrefLang="tr" href="https://mp3paw.mobi/tr/" />
+      <link rel="alternate" hrefLang="vi" href="https://mp3paw.mobi/vi/" />
+      <link rel="alternate" hrefLang="zh-cn" href="https://mp3paw.mobi/zh-cn/" />
+      <link rel="alternate" hrefLang="zh-tw" href="https://mp3paw.mobi/zh-tw/" />
+      <link rel="alternate" hrefLang="ar" href="https://mp3paw.mobi/ar/" />
+      <link rel="alternate" hrefLang="bn" href="https://mp3paw.mobi/bn/" />
+      <link rel="alternate" hrefLang="x-default" href="https://mp3paw.mobi/en10/" />
     </>
   );
 };

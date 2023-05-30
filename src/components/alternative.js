57d24247ca4094e0ecede0556b8603a54e063cec
y2meta <y2meta@proton.me>
2023-05-30 19:44:29 +0530

    fix: alternet changes

--- src/components/alternative.js
+++ src/components/alternative.js
@@ -3,28 +3,28 @@ import React from "react";
 const Alternative = () => {
   return (
     <>
-      <link rel="alternate" hrefLang="en" href="https://mp3paw.mobi/en50/" />
-      <link rel="alternate" hrefLang="de" href="https://mp3paw.mobi/de/" />
-      <link rel="alternate" hrefLang="es" href="https://mp3paw.mobi/es/" />
-      <link rel="alternate" hrefLang="fr" href="https://mp3paw.mobi/fr/" />
-      <link rel="alternate" hrefLang="hi" href="https://mp3paw.mobi/hi/" />
-      <link rel="alternate" hrefLang="id" href="https://mp3paw.mobi/id/" />
-      <link rel="alternate" hrefLang="it" href="https://mp3paw.mobi/it/" />
-      <link rel="alternate" hrefLang="ja" href="https://mp3paw.mobi/ja/" />
-      <link rel="alternate" hrefLang="ko" href="https://mp3paw.mobi/ko/" />
-      <link rel="alternate" hrefLang="my" href="https://mp3paw.mobi/my/" />
-      <link rel="alternate" hrefLang="ms" href="https://mp3paw.mobi/ms/" />
-      <link rel="alternate" hrefLang="tl-ph" href="https://mp3paw.mobi/tl-ph/" />
-      <link rel="alternate" hrefLang="pt" href="https://mp3paw.mobi/pt/" />
-      <link rel="alternate" hrefLang="ru" href="https://mp3paw.mobi/ru/" />
-      <link rel="alternate" hrefLang="th" href="https://mp3paw.mobi/th/" />
-      <link rel="alternate" hrefLang="tr" href="https://mp3paw.mobi/tr/" />
-      <link rel="alternate" hrefLang="vi" href="https://mp3paw.mobi/vi/" />
-      <link rel="alternate" hrefLang="zh-cn" href="https://mp3paw.mobi/zh-cn/" />
-      <link rel="alternate" hrefLang="zh-tw" href="https://mp3paw.mobi/zh-tw/" />
-      <link rel="alternate" hrefLang="ar" href="https://mp3paw.mobi/ar/" />
-      <link rel="alternate" hrefLang="bn" href="https://mp3paw.mobi/bn/" />
-      <link rel="alternate" hrefLang="x-default" href="https://mp3paw.mobi/en10/" />
+      <link rel="alternate" hrefLang="en" href="https://mp3paw.mobi/en101/" />
+      <link rel="alternate" hrefLang="de" href="https://mp3paw.mobi/de2/" />
+      <link rel="alternate" hrefLang="es" href="https://mp3paw.mobi/es2/" />
+      <link rel="alternate" hrefLang="fr" href="https://mp3paw.mobi/fr2/" />
+      <link rel="alternate" hrefLang="hi" href="https://mp3paw.mobi/hi2/" />
+      <link rel="alternate" hrefLang="id" href="https://mp3paw.mobi/id2/" />
+      <link rel="alternate" hrefLang="it" href="https://mp3paw.mobi/it2/" />
+      <link rel="alternate" hrefLang="ja" href="https://mp3paw.mobi/ja2/" />
+      <link rel="alternate" hrefLang="ko" href="https://mp3paw.mobi/ko2/" />
+      <link rel="alternate" hrefLang="my" href="https://mp3paw.mobi/my2/" />
+      <link rel="alternate" hrefLang="ms" href="https://mp3paw.mobi/ms2/" />
+      <link rel="alternate" hrefLang="tl-ph" href="https://mp3paw.mobi/tl-ph2/" />
+      <link rel="alternate" hrefLang="pt" href="https://mp3paw.mobi/pt2/" />
+      <link rel="alternate" hrefLang="ru" href="https://mp3paw.mobi/ru2/" />
+      <link rel="alternate" hrefLang="th" href="https://mp3paw.mobi/th2/" />
+      <link rel="alternate" hrefLang="tr" href="https://mp3paw.mobi/tr2/" />
+      <link rel="alternate" hrefLang="vi" href="https://mp3paw.mobi/vi2/" />
+      <link rel="alternate" hrefLang="zh-cn" href="https://mp3paw.mobi/zh-cn2/" />
+      <link rel="alternate" hrefLang="zh-tw" href="https://mp3paw.mobi/zh-tw2/" />
+      <link rel="alternate" hrefLang="ar" href="https://mp3paw.mobi/ar2/" />
+      <link rel="alternate" hrefLang="bn" href="https://mp3paw.mobi/bn2/" />
+      <link rel="alternate" hrefLang="x-default" href="https://mp3paw.mobi/en101/" />
     </>
   );
 };
